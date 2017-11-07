7652f4bbbf709515ef19d5ed662f4a28c98079cd
outinside147 <g1445396@cc.kyoto-su.ac.jp>
2017-11-07 16:22:37 +0900

    11/7　進捗1

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -23,6 +23,7 @@ int main()
 {
 	// 結果をテキストに出力
 	ofstream ofs("../image/component.txt");
+	ofstream log("../image/log.txt");
 	Mat src = imread("../../../source_images/img1_3_2.jpg",1);
 	Pix *image = pixRead("../../../source_images/img1_3_2.jpg");
 	Mat para_map = src.clone();
@@ -50,13 +51,13 @@ int main()
 
 		// 分割した範囲を切り取ってそれぞれの画像にする
 		Rect rect(box->x, box->y, box->w, box->h);
-		Mat para_img(src, rect);
+		Mat part_img(src, rect);
 		para_row = box->h;
-		//imwrite("../image/splitImages/para_" + to_string(i) + ".png", para_img);
+		//imwrite("../image/splitImages/para_" + to_string(i) + ".png", part_img);
 
 		// 画像に分割した範囲を描写する
 		//rectangle(para_map, Point(box->x, box->y), Point(box->x+box->w, box->y+box->h),Scalar(0,0,255),1,4);
-		//imwrite("../image/splitImages/para_map.png", para_map);
+		//imwrite("../image/splitImages/map/para_map.png", para_map);
 	}
 
 	for (int i = 0; i < line_boxes->n; i++) {
@@ -64,7 +65,15 @@ int main()
 		api->SetRectangle(box->x, box->y, box->w, box->h);
 		char* ocrResult = api->GetUTF8Text();
 		int conf = api->MeanTextConf();
-		ofs << "Textline_Box[" << i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+		ofs << "Textline_Box[" << i << "]: x=" << box->x << "~" << box->x+box->w << ", y=" << box->y << "~" << box->y+box->h << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+		
+		//Rect rect(box->x, box->y, box->w, box->h);
+		//Mat part_img(src, rect);
+		//imwrite("../image/splitImages/line_" + to_string(i) + ".png", part_img);
+
+		//rectangle(line_map, Point(box->x, box->y), Point(box->x+box->w, box->y+box->h),Scalar(0,255,0),1,4);
+		//imwrite("../image/splitImages/map/line_map.png", line_map);
+		
 	}
 
 	for (int i = 0; i < word_boxes->n; i++) {
@@ -72,28 +81,37 @@ int main()
 		api->SetRectangle(box->x, box->y, box->w, box->h);
 		char* ocrResult = api->GetUTF8Text();
 		int conf = api->MeanTextConf();
-		ofs << "Word_Box[" << i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+		ofs << "Word_Box[" << i << "]: x=" << box->x << "~" << box->x + box->w << ", y=" << box->y << "~" << box->y + box->h << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+	
+		//Rect rect(box->x, box->y, box->w, box->h);
+		//Mat part_img(src, rect);
+		//imwrite("../image/splitImages/word_" + to_string(i) + ".png", part_img);
+
+		//rectangle(word_map, Point(box->x, box->y), Point(box->x + box->w, box->y + box->h), Scalar(255, 0, 0), 1, 4);
+		//imwrite("../image/splitImages/map/word_map.png", word_map);
 	}
 
 	for (int i = 0; i < line_boxes->n; i++){
+		int top = para_row;
 		for (int j = 0; j < word_boxes->n; j++){
 			BOX* line_box = boxaGetBox(line_boxes, i, L_CLONE); //1行の座標 (x,y,w,h)
-			BOX* word_box = boxaGetBox(word_boxes, i, L_CLONE); //1単語の座標 (x,y,w,h)
-			int top = para_row;
+			BOX* word_box = boxaGetBox(word_boxes, j, L_CLONE); //1単語の座標 (x,y,w,h)
 	
-			for (int k = line_box->x; k < line_box->x + line_box->w; k++){
-				for (int m = line_box->y; m < line_box->y + line_box->h; m++){
+			for (int k = line_box->x; k <= line_box->x + line_box->w; k++){
+				for (int m = line_box->y; m <= line_box->y + line_box->h; m++){
 					if (k == word_box->x && m == word_box->y){
+						log << "i=" << i << ", j=" << j << ", k=" << k << ", m=" << m << ", top=" << top << endl;
+
 						// 一行の中にある単語の中で最も上にある物の座標を得る
 						if (m < top){
 							top = m;
-							cout << "top=" << top << endl;
+							//log << "top = m = " << m << endl;
 						}
 					}
 				}
 			}
+			//取りだした値を使っての処理　上端に合わせる
 		}
 	}
-
 }
 

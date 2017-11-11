56782dc21467f13c68d70156a3af0a7b0ae0cd0a
outinside147 <g1445396@cc.kyoto-su.ac.jp>
2017-11-11 19:59:02 +0900

    11/11　進捗

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -23,6 +23,9 @@ int main()
 	// 結果をテキストに出力
 	ofstream ofs("../image/component.txt");
 	ofstream log("../image/log.txt");
+	ofstream scn("../image/scn.txt");
+	ofstream lbx("../image/lbox.txt");
+
 	Mat src = imread("../../../source_images/img1_3_2.jpg", 1);
 	Pix *image = pixRead("../../../source_images/img1_3_2.jpg");
 	Mat para_map = src.clone();
@@ -30,7 +33,6 @@ int main()
 	Mat word_map = src.clone();
 
 	int para_row = 0;
-
 
 	TessBaseAPI *api = new TessBaseAPI();
 	api->Init(NULL, "eng");
@@ -60,38 +62,41 @@ int main()
 	}
 
 	// 走査した範囲かどうかを記録する配列
-	Mat scanned_pos = Mat::zeros(src.rows, src.cols,CV_8UC1);
+	Mat scanned_pos = Mat::zeros(src.rows, src.cols, CV_8UC1);
 
-	// 明らかに誤認識しているものを消去する
+	// 明らかに誤認識している行を消去する
 	Boxa* suit_Lboxes = boxaCreate(line_boxes->n);
 	for (int i = 0; i < line_boxes->n; i++) {
 		// 囲う長方形の高さが一定値以上あれば別の行列に格納する
 		BOX* box = boxaGetBox(line_boxes, i, L_CLONE);
 		if (box->h > 20){
 			boxaAddBox(suit_Lboxes, box, L_CLONE);
-			printf("line extracted : i=%3d , box->x=%3d , box->y=%3d , box->w=%3d , box->h=%3d .\n",i,box->x,box->y,box->w,box->h);
+			//printf("line extracted : i=%3d , box->x=%3d , box->y=%3d , box->w=%3d , box->h=%3d .\n",i,box->x,box->y,box->w,box->h);
 		}
 	}
 
 	for (int i = 0; i < suit_Lboxes->n; i++) {
 		BOX* box = boxaGetBox(suit_Lboxes, i, L_CLONE);
-		// 既に走査済かどうか調べる
+		lbx << "suit_Lboxes["<< i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << endl;
+
+		// この行が既に走査済かどうか調べる
 		for(int j = box->x; j <= box->x + box->w; j++){
 			for (int k = box->y; k <= box->y + box->h; k++){
-				if (scanned_pos.at<unsigned char>(j, k) == 0){
-					scanned_pos.at<unsigned char>(j, k) = 1;
+				if (scanned_pos.at<unsigned char>(k, j) == 0){
+					scanned_pos.at<unsigned char>(k, j) = 1;
 				}
-				else if (scanned_pos.at<unsigned char>(j, k) == 1){
-					box = boxaGetBox(suit_Lboxes, i+1, L_CLONE); //エラー
-					printf("break , i = %3d , box->x=%3d , box->y=%3d \n", i,j,k);
+				else if (scanned_pos.at<unsigned char>(k, j) == 1){
+					if (i == suit_Lboxes->n - 1) break;
+					box = boxaGetBox(suit_Lboxes, i+1, L_CLONE);
+					scn << "scan_area i=" << i << ", box->x=" << j << ", box->y=" << k << " break " << endl;		
 					break;
-				}
+				}			
 			}
 		}
 		api->SetRectangle(box->x, box->y, box->w, box->h);
 		char* ocrResult = api->GetUTF8Text();
 		int conf = api->MeanTextConf();
-		ofs << "Textline_Box[" << i << "]: x=" << box->x << "~" << box->x + box->w << ", y=" << box->y << "~" << box->y + box->h << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+		ofs << "Textline_Box[" << i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
 
 		//Rect rect(box->x, box->y, box->w, box->h);
 		//Mat part_img(src, rect);
@@ -107,7 +112,7 @@ int main()
 		BOX* box = boxaGetBox(word_boxes, i, L_CLONE); //para_boxes->n .. number of box in ptr array
 		if (box->h > 3  && box->w > 3){
 			boxaAddBox(suit_Wboxes, box, L_CLONE);
-			printf("word extracted : i=%3d , box->x=%3d , box->y=%3d , box->w=%3d , box->h=%3d .\n", i, box->x, box->y, box->w, box->h);
+			//printf("word extracted : i=%3d , box->x=%3d , box->y=%3d , box->w=%3d , box->h=%3d .\n", i, box->x, box->y, box->w, box->h);
 		}
 	}
 
@@ -117,7 +122,7 @@ int main()
 		api->SetRectangle(box->x, box->y, box->w, box->h);
 		char* ocrResult = api->GetUTF8Text();
 		int conf = api->MeanTextConf();
-		ofs << "Word_Box[" << i << "]: x=" << box->x << "~" << box->x + box->w << ", y=" << box->y << "~" << box->y + box->h << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+		ofs << "Word_Box[" << i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
 
 		//Rect rect(box->x, box->y, box->w, box->h);
 		//Mat part_img(src, rect);

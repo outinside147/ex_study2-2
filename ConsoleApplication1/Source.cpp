61e70c63157fe4d37562d57bb4a0efd13732a584
outinside147 <g1445396@cc.kyoto-su.ac.jp>
2017-11-26 16:41:03 +0900

    11/26　進捗

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -25,6 +25,39 @@ bool  Date_equal(BOX* box1, BOX* box2){
 	return box1->x == box2->x && box1->y == box2->y && box1->w == box2->w && box1->h == box2->h;
 }
 
+// 最も小さい(左上に位置する)単語を求める関数
+BOX* getMinBox(BOX* min_box, BOX* box){
+	if (box == NULL) return min_box;
+	if (box->x <= min_box->x && box->y <= min_box->y){
+		min_box = box;
+	}
+	return min_box;
+}
+
+BOX* getMinBox(BOX* min_box, Boxa* boxes){
+	for (int i = 0; i < boxes->n; i++) {
+		BOX* box = boxaGetBox(boxes, i, L_CLONE);
+		if (box->x < min_box->x && box->y < min_box->y){
+			min_box = box;
+		}
+	}
+	return min_box;
+}
+
+// 次の行の先頭を探す関数
+BOX* getNextRow(BOX* min_box, Boxa* boxes){
+	/* ここから編集 **
+	if(box->x < min_box->x){
+		if(box->y > min_box->y){
+			if(box->y < MIN){
+				MIN = box->y;
+			}
+		}
+	}
+	*/
+
+}
+
 // boxがboxesの中にあればtrue、そうでなければfalseを返す
 bool set_member(BOX* box, Boxa* boxes){
 	int i = 0;
@@ -42,6 +75,7 @@ bool set_member(BOX* box, Boxa* boxes){
 	}
 }
 
+// a_boxesの要素を一つずつ調べ、b_boxesの要素でないなら、c_boxesに加える
 void set_difference(Boxa* a_boxes, Boxa* b_boxes, Boxa* c_boxes){
 	int i, j;
 	for (i = 0, j = 0; i < a_boxes->n; i++){
@@ -60,6 +94,9 @@ int main()
 	int nearest_y = 999;
 
 	Boxa* suit_Wboxes = boxaCreate(6);
+	Boxa* correct_boxes = boxaCreate(50);
+	Boxa* dif_boxes = boxaCreate(50);
+
 	BOX* box0 = boxCreate(119, 65, 9, 6);		// 1行目
 	BOX* box1 = boxCreate(363, 38, 37, 19);		// 
 	BOX* box2 = boxCreate(410, 29, 110, 25);	// 
@@ -69,8 +106,6 @@ int main()
 
 	BOX* min_box = boxCreate(999, 999, 0, 0);
 
-	Boxa* correct_boxes = boxaCreate(50);
-
 	boxaAddBox(suit_Wboxes, box0, L_CLONE);
 	boxaAddBox(suit_Wboxes, box1, L_CLONE);
 	boxaAddBox(suit_Wboxes, box2, L_CLONE);
@@ -78,15 +113,52 @@ int main()
 	boxaAddBox(suit_Wboxes, box4, L_CLONE);
 	boxaAddBox(suit_Wboxes, box5, L_CLONE);
 
-	Boxa* test_boxes = boxaCreate(50);
-	boxaAddBox(test_boxes, box0, L_CLONE);
-	boxaAddBox(test_boxes, box1, L_CLONE);
-	boxaAddBox(test_boxes, box2, L_CLONE);
+	// 最小値を求める(1行目の文章の最も左上)
+	min_box = getMinBox(min_box,suit_Wboxes);
+	boxaAddBox(correct_boxes, min_box, L_CLONE);
+	printf("init : min_box=(%3d,%3d,%3d,%3d) , suit_Wboxes->n=%3d \n", min_box->x, min_box->y, min_box->w, min_box->h, suit_Wboxes->n);
 
-	Boxa* dif_boxes = boxaCreate(50);
+	// 単語をすべて走査するまでループ
+	for (int i = 1; i < suit_Wboxes->n; i++){
+		BOX* box = boxaGetBox(suit_Wboxes, i, L_CLONE);
+		//printf("box=(%3d,%3d,%3d,%3d)\n", box->x, box->y, box->w, box->h);
+		// 得たボックスが空の場合の処理を追加 **
+		// 基準点から右方向に単語を探索
+		for (int j = min_box->x + min_box->w; j <= rparam_end; j++){
+			for (int k = 0; k <= min_box->y + min_box->h; k++){
+				if (j == box->x && k == box->y){
+					printf("word extracted : i=%3d , box=(%3d,%3d)\n", i, j, k);
+					// この行に含まれる単語として配列に格納
+					boxaAddBox(correct_boxes, box, L_CLONE);
+					break_flg = 1;
+					break;
+				}
+			} // ループを脱する
+			if (break_flg == 1) break;
+		}
 
-	set_difference(suit_Wboxes,test_boxes,dif_boxes);
+		if (break_flg == 0){
+			// 基準値か右方向に単語が存在しなかった場合
+			printf("not extracted : i=%3d\n", i);
+			printf("suit_Wboxes->n=%3d , correct_boxes->n=%3d, dif_boxes->n=%3d\n", suit_Wboxes->n, correct_boxes->n, dif_boxes->n);
+			// これまでに見つけた単語を除いた中から、最小(最も左上に位置する)の単語を見つける。それを次の基準値とする
+			set_difference(suit_Wboxes, correct_boxes, dif_boxes);
+			// ここから編集 **
+			printf("next rows point : min_box=(%3d,%3d)\n", min_box->x, min_box->y);
+			boxaAddBox(correct_boxes, min_box, L_CLONE);
+		}
 
+		// 単語が見つかってループを脱した場合
+		if (break_flg == 1){
+			// フラグを初期化
+			break_flg = 0;
+			// 見つけた単語の座標を次の基準値に指定
+			min_box = box;
+			printf("next word point : min_box=(%3d,%3d,%3d,%3d)\n", min_box->x, min_box->y, min_box->w, min_box->h);
+		}
+	}
+
+	printf("dif_boxes->n=%3d\n", dif_boxes->n);
 	for (int i = 0; i < dif_boxes->n; i++) {
 		BOX* box = boxaGetBox(dif_boxes, i, L_CLONE);
 		printf("i=%2d, box->x=%3d, box->y=%3d, box->w=%3d, box->h=%3d\n", i, box->x, box->y, box->w, box->h);

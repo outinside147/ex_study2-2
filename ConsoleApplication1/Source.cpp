5d5bf5bfdbb2ee82604cef6c37608201dff6844d
outinside147 <g1445396@cc.kyoto-su.ac.jp>
2017-12-26 10:07:23 +0900

    12/26　更新1

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -33,6 +33,9 @@ ofstream fls("../image/long_images/line_spacing.txt");
 // BOX型の中心点を格納する構造体
 typedef struct { double x, y;} Box_array;
 
+// 行間の上端と下端を格納する構造体
+typedef struct { int up, bt; } Bet_lines;
+
 // 結果を書きだす関数(map)
 void outputPartImage(BOX* box, string file_name, Mat image, int i){
 	Rect rect(box->x, box->y, box->w, box->h);
@@ -144,8 +147,6 @@ Boxa* setStartPosition(Boxa* boxes){
 	for (int i = 0; i < sort_xboxes->n; i++){
 		BOX* box = boxaGetBox(sort_xboxes, i, L_CLONE);
 		sortx << "sorted_x[" << i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << endl;
-		rectangle(xsort_map, Point(box->x, box->y), Point(box->x + box->w, box->y + box->h), Scalar(255, 255, 0), 1, 4);
-		imwrite("../image/splitImages/map_word_xsort.png", xsort_map);
 	}
 
 	//ソートした配列の先頭から100個の単語を行の先頭候補として別の配列に格納する
@@ -265,44 +266,34 @@ Boxa* findFollowWord(BOX* l_box,Boxa* v_boxes){
 	return line_boxes;
 }
 
-// 最頻値を求める , 入力=全単語列(valid_boxes)
+// 一行分の高さを求める , 入力=全単語列(valid_boxes)
+//全単語列の高さでヒストグラムを作成する。ヒストグラムから最頻値を求め、最頻値のビンの区間中央値を一行分の高さとする。
 double findMode(Boxa* boxes){
 	double class_num = ceil(1 + log2((double)boxes->n)); //階級の数 スタージェスの公式: class_num = 1+log2n round=最も近い整数値に丸める
 	Boxa* sort_asc = boxaSort(boxes, L_SORT_BY_HEIGHT, L_SORT_INCREASING, NULL); //昇順ソート
 	Boxa* sort_desc = boxaSort(boxes, L_SORT_BY_HEIGHT, L_SORT_DECREASING, NULL); //降順ソート
-	int min_h = 0;
-	min_h = boxaGetBox(sort_asc, 0, L_CLONE)->h; //高さの最低値を取得する
-	int max_h = 0;
-	max_h = boxaGetBox(sort_desc, 0, L_CLONE)->h; //高さの最高値を取得する
-	double dstrb = 0;
-	dstrb = double(max_h - min_h) / class_num; //分布の間隔
+	int min_h = boxaGetBox(sort_asc, 0, L_CLONE)->h; //高さの最低値を取得する
+	int max_h = boxaGetBox(sort_desc, 0, L_CLONE)->h; //高さの最高値を取得する
+	double dstrb = double(max_h - min_h) / class_num; //分布の間隔
 
 	printf("boxes->n=%d, class_num=%3lf, max_h=%3d, min_h=%3d, distribution=%lf\n", boxes->n, class_num, max_h, min_h, dstrb);
 
-	double rank = 0;
-	vector<int> hist(class_num);
+	double rank = 0; //ビン
+	vector<int> hist(class_num); //ヒストグラム
 	for (int i = 0; i < boxes->n; i++){
 		BOX* box = boxaGetBox(boxes, i, L_CLONE);
-		rank = floor(box->h / dstrb); //小数点以下切り捨て
-		//printf("%lf\n", rank);
+		rank = floor(box->h / dstrb); //ビンに割り当てる。小数点以下切り捨て
 		if (0 <= rank && rank < class_num){
 			hist[rank]++;
 		}
 	}
 
-	/*
-	for (int i = 0; i < class_num; i++){
-	printf("\n%lf-%lf : %3d人", double((i*dstrb) + 0.1), double((i + 1)*dstrb), hist.at(i));
-	}
-	printf("\n");
-	*/
-
 	int max = 0;
 	int max_i = 0;
 	for (size_t i = 0; i < hist.size(); i++){
 		if (hist.at(i) > max){
-			max = hist.at(i);
-			max_i = i;
+			max = hist.at(i); //最頻値の要素数を求める
+			max_i = i; //最頻値の場所を取得
 		}
 	}
 
@@ -312,55 +303,94 @@ double findMode(Boxa* boxes){
 	rank_max = (max_i + 1)*dstrb; //上限
 	line_h = (rank_min + rank_max) / 2; //上限と下限の平均をとる
 	printf("rank : min=%lf, max=%lf, line_h=%lf\n", rank_min, rank_max, line_h);
-	return line_h*2.5; //一行の高さの2.5倍(二行の高さ)を返り値とする。
+	printf("return value = %lf\n", line_h * 2);
+	return line_h * 2; //一行の高さの2.5倍(二行の高さ)を返り値とする。
 }
 
 // 行間を見つける
-void findLineSpacing(Mat pro_img, Mat l_img, int num){ //入力= 投影画像(Mat),単語画像(Mat)
+Bet_lines findLineSpacing(Mat pro_img, Mat word_img, int num){ //入力= 投影画像(Mat),単語画像(Mat)
 	int up_edge = 0;
 	int bt_edge = 0;
-	Mat map = l_img.clone();
+	int max = 0; //空白行が連続している最大
+	int space_cnt = 0; //空白行をカウントする変数
+	int st_space = 0;
+	Mat map = word_img.clone();
 
+	printf("num=%d\n", num);
 	for (int i = 0; i < pro_img.size().height; i++){
-		if (pro_img.at<int>(i, 0) / 255 == l_img.size().width){ //i行0列の値が単語画像の幅と同じであれば
-			if (up_edge == 0) up_edge = i; //行間の上端を取得
-			bt_edge = i; //行間の下端を取得
+		if (pro_img.at<int>(i, 0) / 255 == word_img.size().width){ //i行0列の値が単語画像の幅と同じであれば
+			space_cnt++;
+			if (space_cnt > max){
+				max = space_cnt;
+				if (st_space == 0) st_space = i; //行間の上端を取得
+			}
+		}
+		else {
+			space_cnt = 0;
 		}
 	}
+	up_edge = st_space;
+	bt_edge = st_space + max;
+	printf("up_edge=%d, bt_edge=%d\n", up_edge, bt_edge);
+
+	//行間の上端、下端が見つからなかった場合、単語を縦方向に2分割する
+	if (up_edge == 0 && bt_edge == 0){
+		up_edge = word_img.size().height / 2;
+		bt_edge = word_img.size().height / 2;
+	}
+	else{
+		bt_edge--;
+	}
+
 	//上端から文字を囲う
-	rectangle(map, Point(0, 0), Point(l_img.size().width, up_edge), Scalar(0, 0, 255), 1, 1);
+	rectangle(map, Point(0, 0), Point(word_img.size().width, up_edge), Scalar(0, 0, 255), 1, 1);
+	Rect up_rect(Point(0, 0), Point(word_img.size().width, up_edge));
+	Mat up_img(word_img, up_rect);
+	imwrite("../image/long_images/up_images/up_img_" + to_string(num) + ".png", up_img);
 	//下端から文字を囲う
-	rectangle(map, Point(0, bt_edge), Point(l_img.size().width, l_img.size().height), Scalar(0, 0, 255), 1, 1);
+	rectangle(map, Point(0, bt_edge), Point(word_img.size().width, word_img.size().height), Scalar(0, 0, 255), 1, 1);
+	Rect bt_rect(Point(0, bt_edge), Point(word_img.size().width, word_img.size().height));
+	Mat bt_img(word_img, bt_rect);
+	imwrite("../image/long_images/bt_images/bt_img_" + to_string(num) + ".png", bt_img);
+	//結果をファイルへ出力
 	fls << "i=" << num << endl;
-	fls << "up_box=(0, 0) -- (" << l_img.size().width << ", " << up_edge << ")" << endl;
-	fls << "bt_box=(0, " << bt_edge << ") -- (" << l_img.size().width << ", " << l_img.size().height << ")" << endl << endl;
-	//imwrite("../image/long_images/map_ls_" + to_string(num) + ".png", map);
+	fls << "up_box=(0, 0) -- (" << word_img.size().width << ", " << up_edge << "), up_box.h=" << up_edge << endl;
+	fls << "bt_box=(0, " << bt_edge << ") -- (" << word_img.size().width << ", " << word_img.size().height << "), bt_box.h=" << word_img.size().height - bt_edge << endl << endl;
+	imwrite("../image/long_images/map_ls_" + to_string(num) + ".png", map);
+
+	Bet_lines edge = { up_edge, bt_edge };
+	printf("return\n");
+	return  edge;
 }
 
 // 縦長の画像を分割する
-void divideImage(Boxa* boxes, Mat img){
+Boxa* divideImage(Boxa* boxes, Mat img){
 	ofstream pjt("../image/long_images/projection.txt");
 	ofstream lng("../image/long_images/long.txt");
-	ofstream gry("../image/long_images/gry.txt");
 
 	Mat gray_img; //グレースケール画像
 	Mat bn_img; //二値化画像
 
+	Boxa* edge_box = boxaCreate(100); //分割した抽出枠を格納する配列
+
 	cvtColor(img, gray_img, CV_RGB2GRAY); //元画像をグレースケール画像に変更する
 	threshold(gray_img, bn_img, 0, 255, THRESH_BINARY | THRESH_OTSU); //大津の方法で二値化する
-	//imwrite("../image/long_images/bn_image.png", bn_img);
+	imwrite("../image/long_images/bn_image.png", bn_img);
 
 	for (int i = 0; i < boxes->n; i++){
 		BOX* box = boxaGetBox(boxes, i, L_CLONE);
 		Rect rect(box->x, box->y, box->w, box->h);
 		Mat long_img(bn_img, rect);
-		lng << "i=" << i << ", long_img.width=" << long_img.size().width << ", long_img.height=" << long_img.size().height << ", long_img=" << endl << long_img / 255 << endl;
 		Mat project_img; //投影結果
-		//imwrite("../image/long_images/long_" + to_string(i) + ".png", long_img);
 		reduce(long_img, project_img, 1, CV_REDUCE_SUM, CV_32S); //列ごとの合計を求める,出力はint型
 		pjt << "i=" << i << ", long_img.width=" << long_img.size().width << ", long_img.height=" << long_img.size().height << ", project_img=" << endl << project_img / 255 << endl;
-		findLineSpacing(project_img, long_img, i);
+		Bet_lines edge = findLineSpacing(project_img, long_img, i);
+		BOX* up_box = boxCreate(box->x, box->y, box->w, edge.up);
+		BOX* bt_box = boxCreate(box->x, box->y + edge.bt, box->w, box->h - edge.bt);
+		boxaAddBox(edge_box, up_box, L_CLONE);
+		boxaAddBox(edge_box, bt_box, L_CLONE);
 	}
+	return edge_box;
 }
 
 int main()
@@ -398,12 +428,14 @@ int main()
 
 	Mat valid_map = mat_para_img.clone();
 	Mat sentence_map = mat_para_img.clone();
+
 	Boxa* valid_boxes = boxaCreate(word_boxes->n); //小さな抽出枠を取り除いた単語列
 	Boxa* long_boxes = boxaCreate(word_boxes->n); //縦長の単語列
-	Boxa* target_boxes = boxaCreate(500); //valid_boxesと縦長の単語列を分割した単語列を合わせたもの
-	Boxa* leading_boxes = boxaCreate(100); //先頭単語列
-	Boxa* line_boxes = boxaCreate(100); //一行の単語列
-	Boxaa* sentence_boxas = boxaaCreate(100); //全行の単語列
+	Boxa* div_boxes = boxaCreate(word_boxes->n); //分割単語列
+	Boxa* tgt_boxes = boxaCreate(word_boxes->n * 2); //valid_boxesと縦長の単語列を分割した単語列を合わせたもの
+	Boxa* leading_boxes = boxaCreate(200); //先頭単語列
+	Boxa* line_boxes = boxaCreate(200); //一行の単語列
+	Boxaa* sentence_boxas = boxaaCreate(200); //全行の単語列
 
 	// 明らかに誤認識している単語を消去する
 	for (int i = 0; i < word_boxes->n; i++) {
@@ -420,29 +452,56 @@ int main()
 		char* ocrResult = api2->GetUTF8Text();
 		int conf = api2->MeanTextConf();
 		content << "Word_Box[" << i << "]: x=" << box->x << ", y=" << box->y << ", w=" << box->w << ", h=" << box->h << ", confidence=" << conf << ", text= " << ocrResult << endl;
+		outputPartImage(box, "../image/splitImages/word_", mat_para_img, i);
 		rectangle(valid_map, Point(box->x, box->y), Point(box->x + box->w, box->y + box->h), Scalar(0, 0, 255), 1, 4);
 		imwrite("../image/splitImages/map_word_valid.png", valid_map);
 	}
 
 	// 二行に渡る抽出枠を縦方向に分割する
 	double two_row_length = findMode(valid_boxes); //二行の長さを設定
+	//最頻値とその場所を表示
+	printf("two_row_length=%lf\n", two_row_length);
 	for (int i = 0; i < valid_boxes->n; i++) { //上記の値を使って、二行の長さの検出枠を抽出する
 		BOX* box = boxaGetBox(valid_boxes, i, L_CLONE);
 		if (box->h > two_row_length){
 			boxaAddBox(long_boxes, box, L_CLONE);
 		}
 	}
-	divideImage(long_boxes, mat_para_img); //縦長の画像を分割する
+	
+	//縦長の画像を分割する
+	div_boxes = divideImage(long_boxes, mat_para_img); //縦長の画像を分割する
+
+	//分割後の画像を探索配列に格納する
+	for (int i = 0; i < div_boxes->n; i++){
+		BOX* box = boxaGetBox(div_boxes, i, L_CLONE);
+		if (box->h > 10){
+			boxaAddBox(tgt_boxes, box, L_CLONE);
+		}
+	}
+	//分割前の単語(縦長の画像)を探索対象から外す
+	for (int i = 0; i < valid_boxes->n; i++){
+		BOX* box = boxaGetBox(valid_boxes, i, L_CLONE);
+		if (box->h < two_row_length){ 
+			boxaAddBox(tgt_boxes, box, L_CLONE);
+		}
+	}
+
+	Mat all_map = mat_para_img.clone();
+	//全単語を出力する
+	for (int i = 0; i < tgt_boxes->n; i++){
+		BOX* box = boxaGetBox(tgt_boxes, i, L_CLONE);
+		rectangle(all_map, Point(box->x, box->y), Point(box->x + box->w, box->y + box->h), Scalar(255, 0, 255), 1, 1);
+		imwrite("../image/long_images/all_map_word.png", all_map);
+	}
 
 	// 全ての行の先頭単語を見つける , 入力=全単語列
-	leading_boxes = setStartPosition(valid_boxes);
+	leading_boxes = setStartPosition(tgt_boxes);
 
 	// 先頭単語の数だけ右方向の探索処理を行う
 	for (int i = 0; i < leading_boxes->n; i++){
 		BOX* box = boxaGetBox(leading_boxes, i, L_CLONE);
-		Boxa* searched_boxes = boxaCreate(500);
 		//先頭単語から右方向に向かって次の単語を探す , 入力=先頭単語,全単語列
-		line_boxes = findFollowWord(box, valid_boxes);
+		line_boxes = findFollowWord(box, tgt_boxes);
 		boxaaAddBoxa(sentence_boxas, line_boxes, L_CLONE);
 		//valid_boxesからline_boxesの要素を取り除いて返す。これを次の探索対象とする , 重複フラグ
 		//valid_boxes = setDiff(valid_boxes, line_boxes);
